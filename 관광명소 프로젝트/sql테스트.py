8264994c0abde1950c57a8e32d1e6178f3613f34
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-01-10 17:55:37 +0900

    Update sql테스트.py

--- 관광명소 프로젝트/sql테스트.py
+++ 관광명소 프로젝트/sql테스트.py
@@ -1,8 +1,21 @@
 import pymysql
 
+conn,cur = None,None
+data1,data2,data3,data4 = "","","",""
+sql = ""
+
 conn = pymysql.connect(host='127.0.0.1',user='root',password='0000',db='solodb',charset='utf8')
 cur = conn.cursor()
 
+while(True):
+    data1 = input("사용자 아이디 ==> ")
+    if data1 == "":
+        break
+    data2 = input("사용자 이름 ==> ")
+    data3 = input("사용자 이메일 ==> ")
+    data4 = input("사용자 출생년도 ==> ")
+    sql = "INSERT INTO userTable VALUES('"+data1+"','"+data2+"','"+data3+"',"+data4+")"
+    cur.execute(sql)
 
-cur.execute("INSERT INTO userTable VALUES('hong','홍지윤','hong@naver.com',1996)")
 conn.commit()
+conn.close()    
