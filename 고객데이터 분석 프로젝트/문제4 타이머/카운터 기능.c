28e1c4b1d3aefbec25c9a5e452339105ff23bcd9
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-04-25 08:09:49 +0900

    Update 카운터 기능.c

--- 고객데이터 분석 프로젝트/문제4 타이머/카운터 기능.c
+++ 고객데이터 분석 프로젝트/문제4 타이머/카운터 기능.c
@@ -7,36 +7,43 @@ volatile int count = 0;
 volatile int state = 0;
 volatile int state2 = 0;
 volatile int count2 = 0;
-ISR(TIMER0_COMP_vect)
+
+ISR(TIMER0_OVF_vect)
 {
 	count++;
-	if(count == 64){
+	if (count == 64) {
 		count = 0;
 		state = !state;
-		if(state) PORTC = 0x01;
-		else PORTC = 0x00;
+		if (state) PORTC |= (1 << PORTC0);
+		else PORTC &= ~(1 << PORTC0);
 	}
 }
+
 ISR(TIMER2_COMP_vect)
 {
 	count2++;
-	if (count2 == 96) {
+
+	if (count2 == 144) {
 		count2 = 0;
 		state2 = !state2;
-		if (state2) PORTC = 0x03;
-		else PORTC = 0x00;
+		if (state2) PORTC |= (1 << PORTC1);
+		else PORTC &= ~(1 << PORTC1);
 	}
 }
+
 int main(void)
 {
-	DDRC = 0x01;
-	PORTC = 0x00;
-	TCCR0 |= (1<<WGM01) | (1<<CS02) | (1<<CS01) | (1<<CS00);
-	TCCR2 |= (1<<WGM01) | (1<<CS02) | (1<<CS01) | (0<<CS00);
+	DDRC |= (1 << DDC0) | (1 << DDC1);
+	PORTC &= ~((1 << PORTC0) | (1 << PORTC1));
+
+	TCCR0 |= (1<<CS02) | (1<<CS01) | (1<<CS00);
+	TCCR2 |= (1 << CS22) | (0 << CS21) | (1 << CS20);
+	TIMSK |= (1 << TOIE0) | (1 << OCIE2);
 	OCR0 = 128;
-	
-	TIMSK |= (1<< OCIE0);
+	OCR2 = 71;
 	sei();
-	while(1){ }
+
+	while (1) { }
+
 	return 0;
 }
