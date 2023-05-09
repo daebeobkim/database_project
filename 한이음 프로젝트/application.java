8a6b42d2a9ddd651fabecbca1963be9da4727001
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-05-09 20:48:21 +0900

    Update application.java

--- 한이음 프로젝트/application.java
+++ 한이음 프로젝트/application.java
@@ -1,6 +1,8 @@
 
 public class application {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) {//줄여서 psvm
+        String name = "나도코딩";
+        int hour = 15;
+        System.out.println(name + "님, 배송이 시작됩니다. "+hour+"시에 방문 예정입니다.");//줄여서 sout
     }
 }
