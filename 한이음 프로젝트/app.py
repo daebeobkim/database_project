a7d77f1be1ec349569ab44c535bf9874c9fba5ea
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-06-21 19:23:40 +0900

    Update app.py

--- 한이음 프로젝트/app.py
+++ 한이음 프로젝트/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import pymysql
+
+db = pymysql.connect(host="localhost", user="root", passwd="0000", db="practice", charset="utf8")
+cur = db.cursor()
+sql = 'SELECT * from customer'
+cur.execute(sql)
+
+data_list = cur.fetchall()
+
+print(data_list[0])
+print(data_list[1])
+print(data_list[2])
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html',data_list=data_list)
+    
 if __name__ == '__main__':
     app.run()
+    
