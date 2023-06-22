0a0c3f897bbe786160d27b77bf08f757b7d8cf87
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-06-22 14:40:11 +0900

    Update app.py

--- 한이음 프로젝트/app.py
+++ 한이음 프로젝트/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 import pymysql
+from flask_socketio import SocketIO
+import time
+
+socketio = SocketIO(logger=True,engineio_logger=True)
+
 
 db = pymysql.connect(host="192.168.0.40", user="root", passwd="1234", db="CAP2", charset="utf8")
 cur = db.cursor()
@@ -8,17 +13,16 @@ cur.execute(sql)
 
 data_list = cur.fetchall()
 
-print(data_list[0])
-print(data_list[1])
-print(data_list[2])
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    time.sleep(1)
     return render_template('index.html',data_list=data_list)
     
+
+
 host_addr = "0.0.0.0"
 port_num = "5000"
 
