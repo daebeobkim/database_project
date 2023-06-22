099622f3635c544127508b51a5bd3aea93927ba2
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-06-22 20:07:59 +0900

    Update app.py

--- 한이음 프로젝트/app.py
+++ 한이음 프로젝트/app.py
@@ -1,10 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql
-from flask_socketio import SocketIO
-import time
-
-socketio = SocketIO(logger=True,engineio_logger=True)
-
 
 db = pymysql.connect(host="192.168.0.40", user="root", passwd="1234", db="CAP2", charset="utf8")
 cur = db.cursor()
@@ -15,16 +10,20 @@ data_list = cur.fetchall()
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-    return render_template('index.html',data_list=data_list)
-    
+    if request.method == 'POST':
+        # POST 요청 처리
+        # 요청 파라미터 받기
+        param = request.form.get('param_name')
 
+        # 처리 로직 구현
+        # ...
+
+        return render_template('index.html', data_list=data_list, param=param)
 
 host_addr = "0.0.0.0"
 port_num = "5000"
 
 if __name__ == '__main__':
-    app.run(host=host_addr,port=port_num,debug=True)
-    
+    app.run(host=host_addr, port=port_num, debug=True)
