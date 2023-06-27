4013855ef96fc09579fd597629c06fae77f30d06
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-06-27 17:04:21 +0900

    Create test.py

---
+++ 한이음 프로젝트/test.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify
+import pymysql
+
+db = pymysql.connect(host="192.168.0.40", user="root", passwd="1234", db="CAP2", charset="utf8")
+cur = db.cursor()
+sql = 'SELECT * from han'
+cur.execute(sql)
+
+data_list = cur.fetchall()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify(data_list=data_list)
+
+
+host_addr = "0.0.0.0"
+port_num = "5000"
+
+if __name__ == '__main__':
+    app.run(host=host_addr, port=port_num, debug=True)
